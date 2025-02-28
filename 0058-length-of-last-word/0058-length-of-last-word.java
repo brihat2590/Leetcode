3125f122c75e27b8dd18e5c2a8fafbbacac8a02e
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2025-02-28 14:42:05 +0530

    Time: 0 ms (100%), Space: 41.6 MB (90.39%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        int output=0;
+        s=s.trim();
+        for(int i=s.length()-1;i>=0;i--){
+
+
+            if(s.charAt(i)==' '){
+                break;
+            }
+            else{
+                output++;
+            }
+            
+            
+
+        }
+        return output;
+        
+        
+    }
+}
