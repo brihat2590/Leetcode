494f3b3aaba9a386e028cfb06eff459027f0f490
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2025-03-10 09:17:40 +0530

    Time: 1 ms (99.09%), Space: 44.6 MB (86.25%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length()>magazine.length()) return false;
+        int[] alphabets=new int[26];
+        for(char ch:magazine.toCharArray()){
+            alphabets[ch -'a']++;
+        }
+        for(char ch:ransomNote.toCharArray()){
+            if(alphabets[ch-'a']==0){
+                return false;
+                
+
+            }
+            alphabets[ch-'a']--;
+
+        }
+        return true;
+
+
+        
+    }
+}
