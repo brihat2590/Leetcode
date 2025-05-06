ef33ff93f6848a84b119fdf8ce77bd88cae546c1
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2025-05-06 21:26:27 +0530

    Time: 3 ms (56.43%), Space: 53.1 MB (49.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int maxFreq=-1;
-        int maxNumber=0;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // int maxFreq=-1;
+        // int maxNumber=0;
 
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-            if(map.get(num)>maxFreq){
-                maxFreq=map.get(num);
-                maxNumber=num;
-            }
-        }
-        return maxNumber;
+        // for(int num:nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        //     if(map.get(num)>maxFreq){
+        //         maxFreq=map.get(num);
+        //         maxNumber=num;
+        //     }
+        // }
+        // return maxNumber;
+
+
+
+        //simple method
+        Arrays.sort(nums);
+        //sort vayo increasing order ma
+        int n=nums.length;
+        return nums[n/2];
+
     }
 }
