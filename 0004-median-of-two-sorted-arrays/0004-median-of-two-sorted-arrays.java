1704c64df3ade38c0bc71e931b25c8f28fc8215d
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2025-12-19 22:36:08 +0530

    Time: 7 ms (21.7%), Space: 49.3 MB (8.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,23 +3,26 @@ class Solution {
 
         int[] merged=new int[nums1.length+nums2.length];
         int k=0;
-        for(int i=0;i<nums1.length;i++){
+        // k le index hercha;
+
+        for(int i=0;i<=nums1.length-1;i++){
             merged[k++]=nums1[i];
         }
-        for (int i=0;i<nums2.length;i++){
+        for(int i=0;i<=nums2.length-1;i++){
             merged[k++]=nums2[i];
+
+        }
+        Arrays.sort(merged);
+        int n=merged.length;
+        //the array can be odd or even so case;
+        if (n % 2 != 0) {
+            return merged[n / 2];
         }
 
-        Arrays.sort(merged);
-
-        int n=merged.length;
-        if(n%2==0){
+        else {
             return (merged[n / 2 - 1] + merged[n / 2]) / 2.0;
         }
-        else{
-            return merged[n/2];
-        }
+
         
     }
-
 }
