75d2eea9bfbae2a8c884e7db385ca8211c5d6ec0
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2026-02-23 22:33:44 +0530

    Time: 4 ms (42.22%), Space: 43.4 MB (41.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,32 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stc=new Stack<>();
 
-        for (char c : s.toCharArray()) {
-            if (c == '{' || c == '(' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false; // Closing bracket without opening
-                
-                char top = stack.pop(); // Pop the top element
-                
-                // Check if brackets match
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
+        for(Character ch:s.toCharArray()){
+            if(ch=='('|| ch=='{'||ch=='['){
+                stc.push(ch);
+            }
+
+            else{
+                if(stc.isEmpty()){
+                    return false;
+                }
+                char top=stc.pop();
+                if(ch==')' && top!='('){
+                    return false;
+                }
+                if(ch=='}' && top!='{'){
+                    return false;
+                }
+                if(ch==']' && top!='['){
                     return false;
                 }
             }
         }
-
-        return stack.isEmpty(); // Ensure all brackets are matched
+        if(stc.isEmpty()){
+            return true;
+        }
+        return false;
     }
-}
-
     
+}
