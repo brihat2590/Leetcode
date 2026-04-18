5150f3e34705042f7952ac2adbbaee9c9cee6926
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2026-04-18 15:03:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,14 +1,12 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
         int count=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
+        for(int num:nums){
+            count=count+map.getOrDefault(num,0);
+            map.put(num,map.getOrDefault(num,0)+1);
+            
         }
         return count;
-        
     }
 }
