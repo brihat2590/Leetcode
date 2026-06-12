fce6c3b29589efe464b97e099c124763bef39125
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2026-06-12 23:21:53 +0530

    Time: 1 ms (96.37%), Space: 42.9 MB (58.26%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,17 +1,26 @@
 class Solution {
     public int countGoodSubstrings(String s) {
 
-        //contiguos vaneko xa so direct loop
+        int left=0;
         int count=0;
-        for(int i=0;i+2<s.length();i++){
-            char a=s.charAt(i),b=s.charAt(i+1),c=s.charAt(i+2);
-            //contiguos xan sab
-            if(a!=b && b!=c &&c!=a){
-                count++;
-                //direct vetiyo count
+        for(int right=0;right<s.length();right++){
+            if(right-left+1==3){
+                //3 ta val huna paryo right 2 index ma pugesi
+                //hunxa i,i+1,i+2
+                 
+                 //left aghi badhaune
+                char a=s.charAt(left),b=s.charAt(left+1),c=s.charAt(left+2);
+
+                if(a!=b && b!=c && c!=a){
+                    count++;
+
+                }
+                left++;
             }
+            
         }
         return count;
+        
         
     }
 }
