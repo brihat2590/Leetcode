7aca228a7abda106537d00ab769dbbb517d50029
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2026-06-20 23:14:10 +0530

    Time: 0 ms (100%), Space: 45.1 MB (19.55%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,15 +1,31 @@
 class Solution {
     public int findMin(int[] nums) {
-    
+        if(nums.length==1) return nums[0];
+        
     //best approach to find the inflection point
 
-    for(int i=1;i<nums.length;i++){
-        if(nums[i]<nums[i-1]){
-            return nums[i];
+    int left=0;
+    int ans=Integer.MAX_VALUE;
+    int right=nums.length-1;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==nums[left] && nums[mid]==nums[right]){
+            ans=Math.min(ans,nums[left]);
+            left++;
+            right--;
+            continue;
         }
-    }
-    //base 
-    return nums[0];
+        if(nums[left]<=nums[mid]){
+            ans=Math.min(ans,nums[left]);
+            left=mid+1;
+        }
+        else{
+            ans=Math.min(ans,nums[mid]);
+            right=mid-1;
+            
+        }
+
         
     }
-}
+    return ans;
+}}
