99402ce21ecb2f5185f0acbdd9ebdccc55e71012
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2026-06-22 07:59:54 +0530

    Time: 7 ms (100%), Space: 47.6 MB (61.93%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,20 +1,21 @@
 class NumArray {
-    int[] nums;
-    
+    int[] prefix;
 
     public NumArray(int[] nums) {
-        this.nums=nums;
+        prefix=new int[nums.length];
+        prefix[0]=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
         
     }
     
     public int sumRange(int left, int right) {
-        int sum=0;
-        for(int i=left;i<=right;i++){
-            sum+=nums[i];
+        if(left==0){
+            return prefix[right];
         }
-
-        return sum;
-        
+        return prefix[right]-prefix[left-1];
     }
 }
 
