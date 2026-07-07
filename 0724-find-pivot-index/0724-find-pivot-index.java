3d91156ddc5bd038eb55f540a8e8f7838697aea3
Brihatbir Thapa <thapabrihatbir321@gmail.com>
2026-07-08 00:14:25 +0530

    Time: 1 ms (98.63%), Space: 47.1 MB (88.63%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        for(int num:nums){
+            sum=sum+num;
+        }
+        int leftSum=0;
+        for(int i=0;i<nums.length;i++){
+            
+            int rightSum=sum-leftSum-nums[i];
+
+            if(leftSum==rightSum){
+                return i;
+            }
+            leftSum=leftSum+nums[i];
+        }
+        return -1;
+        
+    }
+}
